c7dfb77a9de8ecadeafe869c1a26090ec91f6185
Beto Noronha <beto_noronha@live.com>
2021-03-16 22:05:15 -0300

    fix variables example

--- api/config.py
+++ api/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
         API_ENABLE_DELETE_IMG = os.getenv('API_ENABLE_DELETE_IMG', 'false').lower() == 'true'
         
         REGISTRY_URL = os.getenv('REGISTRY_URL', 'http://localhost:5000/v2')
-        OAUTH_URL = os.getenv('OAUTH_URL', 'http://localhost:5001')
+        OAUTH_URL = os.getenv('OAUTH_URL', 'http://localhost:5001/token')
 
         FRONT_FOLDER_DIST = os.getenv('FRONT_FOLDER_DIST', '/repoD/front-end/')
         FRONT_APP_NAME = os.getenv('FRONT_APP_NAME', 'Docker Registry UI')
